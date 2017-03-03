08ebf1393c4afcdb081c96633ad9cda207a3c55c
Yuriy <ususus_russ@mail.ru>
2017-03-03 14:34:02 +0300

    Скомпилированно

--- TestApp/TestApp.cpp
+++ TestApp/TestApp.cpp
@@ -7,7 +7,9 @@ using namespace std;
 
 int main()
 {
-	cout << "Why the Cirillic does not display? да сколько можно";
+	cout << "Why the Cirillic does not display? да сколько можно\n";
+
+	system("pause");
     return 0;
 }
 
